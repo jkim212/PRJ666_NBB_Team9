b813116bb4f9699aa34433fc2bc5095423f9b273
SangWoo-Shin <90230423+SangWoo-Shin@users.noreply.github.com>
2024-06-21 00:00:37 -0400

    Update freeboardRoutes.js

--- src/routes/freeboardRoutes.js
+++ src/routes/freeboardRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const multer = require('multer');
 const fs = require('fs');
 const Post = require('../models/post');
+const Comment = require('../models/comments');
+
 const uploadMiddleware = multer({ dest: 'uploads/' });
 
 router.put('/freeboard/edit/:id', uploadMiddleware.single('file'), async (req, res) => {
@@ -55,6 +57,39 @@ router.post('/freeboard', uploadMiddleware.single('file'), async (req, res) => {
     res.json(postDoc);
     console.log(postDoc);
 });
+
+router.post('/freeboard/:id/comment', async (req, res) => {
+    const { id } = req.params;
+    const { body } = req.body; 
+
+    try {
+        if (!body) {
+            return res.status(400).json({ error: 'Body is required fields' });
+        }
+
+        const newComment = await Comment.create({
+            postId: id,
+            //userId,
+            body
+        });
+
+        res.status(201).json(newComment);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Server error' });
+    }
+});
+
+router.get('/freeboard/:id/comments', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const comments = await Comment.find({ postId: id }).sort({ createdAt: -1 });
+        res.json({ comments });
+    } catch (error) {
+        console.error(`Error fetching comments for post ID ${id}:`, error);
+        res.status(500).json({ error: 'Server error' });
+    }
+    });
   
 router.get('/freeboard', async (req,res) => {
     res.json(await Post.find().sort({createdAt: -1}));
@@ -65,7 +100,6 @@ router.get('/freeboard/:id', async (req, res) => {
     const postDoc = await Post.findById(id);
     res.json(postDoc);
 })
-
 
 router.delete('/freeboard/:id', async (req, res) => {
     const { id } = req.params;
