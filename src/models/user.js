91a042c7c4cb9330c7729a3dd6ed015ddad24756
SangWoo-Shin <90230423+SangWoo-Shin@users.noreply.github.com>
2024-07-17 09:06:31 -0400

    Update user.js

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const {Schema, model} = mongoose;
 
 const userSchema = new mongoose.Schema({
 email:{
@@ -11,11 +11,30 @@ role:{
   type:String,
   required:true,
 },
-
 created_at: {
   type: Date,
   default: Date.now,
 },
+profile_picture: {
+  type: String,
+},
+first_name: {
+  type: String,
+},
+last_name: {
+  type: String,
+},
+entrance_year: {
+  type: Number,
+},
+program: {
+  type: String,
+},
+bio: {
+  type: String,
+},
+posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
+activities: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Activity' }]
 });
 
 const User = mongoose.model('User', userSchema);
