564e859741e3bd3be428514d83eb4be67171e263
Juhan Kim <dyslexia0212@gmail.com>
2024-08-02 17:29:50 -0400

    Update notificationRoutes.js

--- src/routes/notificationRoutes.js
+++ src/routes/notificationRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authenticate = require('../middleware/authenticate');
 const notificationController = require('../controllers/notificationController');
 
-router.post('/create', notificationController.createNotification);
+router.post('/notifications/create', notificationController.createNotification);
 router.get('/notifications', notificationController.getNotifications);
 
 module.exports = router;
