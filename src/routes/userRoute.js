3f739c005392953387becd19ee84618d6c864944
Namandeep Singh Wadhwa <naman23156@gmail.com>
2024-08-08 11:06:57 -0400

    userbyid added

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -4,5 +4,6 @@ const userController = require('../controllers/userController');
 const authenticate = require('../middleware/authenticate');
 
 router.get("/getUser", authenticate, userController.getUserInfo);
+router.get("/getUser/:id", authenticate, userController.getUserbyId);
 
 module.exports = router;
